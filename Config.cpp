4e85acbb2d10fb74d3664dffdb2bc20fa2848ff3
The Sudoer <thesudoer2@gmail.com>
2026-03-26 13:33:04 +0330

    Update config

--- Config.cpp
+++ Config.cpp
@@ -1,11 +1,12 @@
 #include "Config.h"
 
+#include <cctype>
 #include <cstdlib>
 
-#include <iostream>
 #include <string>
 #include <tuple>
 
+#include "FileWrapper.h"
 #include "Global.h"
 #include "JsonParser.h"
 
@@ -22,42 +23,45 @@ std::unordered_set<std::string> ConfigParser::_valid_configs{PRUNE_BIND_MOUNTS_C
                                                              PRUNE_PATHS_CONFIG,
                                                              PRUNE_NAMES_CONFIG};
 
-ConfigParser::ConfigParser(const std::string &config_file) noexcept : _prune_bind_mounts(false)
+ConfigParser::ConfigParser(const std::string &config_file, std::string *err_msg) noexcept : _prune_bind_mounts(false)
 {
-    bool parser_res = parse(config_file);
+    bool parser_res = parse(config_file, err_msg);
     if (!parser_res)
     {
         exit(EXIT_FAILURE);
     }
 }
 
-bool ConfigParser::parse(const std::string &config_file) noexcept
+bool ConfigParser::parse(const std::string &config_file, std::string *err_msg) noexcept
 {
     JsonParser json_parser(config_file);
 
-    std::string err_msg;
-    if (bool parse_res = json_parser.parse(&err_msg); !parse_res)
+    std::string jp_err_msg;
+    if (bool parse_res = json_parser.parse(&jp_err_msg); !parse_res)
     {
-        std::cerr << "FindSymbol::ConfigParser -- Parsing Json file failed:\n";
-        std::cerr << err_msg << '\n';
-        exit(EXIT_FAILURE);
+        *err_msg = jp_err_msg;
+        return false;
     }
 
-    bool validation_status = false;
-    std::tie(validation_status, err_msg) = validate_config(json_parser);
+    auto [validation_status, validation_err_msg] = validate_config(json_parser);
     if (!validation_status)
     {
-        std::cerr << "FindSymbol::ConfigParser -- Validating Json file failed:\n";
-        std::cerr << err_msg << '\n';
-        exit(EXIT_FAILURE);
+        *err_msg = validation_err_msg;
+        return false;
     }
 
-    extract_config(json_parser);
+    auto [extraction_status, extraction_err_msg] = extract_config(json_parser);
+    if (!extraction_status)
+    {
+        *err_msg = extraction_err_msg;
+        return false;
+    }
 
     return true;
 }
 
-std::pair<bool, std::string> ConfigParser::validate_config(const JsonParser &json_data) const noexcept // NOLINT
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+std::pair<bool, std::string> ConfigParser::validate_config(const JsonParser &json_data) const noexcept
 {
     if (!json_data.is_object())
     {
@@ -83,40 +87,90 @@ std::pair<bool, std::string> ConfigParser::validate_config(const JsonParser &jso
     return {true, {}};
 }
 
-void ConfigParser::extract_config(const JsonParser &json_data) noexcept
+std::pair<bool, std::string> ConfigParser::extract_config(const JsonParser &json_data) noexcept
 {
     for (const auto &item : json_data.items())
     {
         const auto &key = item.key();
         const auto &value = item.value();
 
-        store_config(key, value.get_cref_value<std::string>());
+        auto [store_status, store_err_msg] = store_config(key, value.get_cref_value<std::string>());
+        if (!store_status)
+        {
+            return {store_status, store_err_msg};
+        }
+    }
+
+    return {true, ""};
+}
+
+expected<bool, std::string> to_bool(const std::string &str)
+{
+    if (strcasecmp("true", str.c_str()) == 0)
+    {
+        return true;
+    }
+    else if (strcasecmp("false", str.c_str()) == 0)
+    {
+        return false;
+    }
+
+    return unexpected<std::string>("Invalid value as boolean: " + str);
+}
+
+/* Add values from space-separated STR to LIST */
+void break_string(std::vector<std::string> &list, const std::string &str)
+{
+    std::string extracted_str;
+    extracted_str.resize(str.size());
+
+    for (char ch : str) // NOLINT(readability-identifier-length)
+    {
+        if (std::isspace(ch) != 0)
+        {
+            if (!extracted_str.empty())
+            {
+                list.push_back(extracted_str);
+                extracted_str.clear();
+            }
+
+            continue;
+        }
+
+        extracted_str += ch;
+    }
+
+    if (!extracted_str.empty())
+    {
+        list.push_back(extracted_str);
     }
 }
 
-bool to_bool(const std::string &str)
-{
-    return strcasecmp("true", str.c_str()) == 0;
-}
-
-void ConfigParser::store_config(const std::string &key, std::string value) noexcept
+std::pair<bool, std::string> ConfigParser::store_config(const std::string &key, std::string value) noexcept
 {
     if (key == PRUNE_BIND_MOUNTS_CONFIG)
     {
-        _prune_bind_mounts = to_bool(value);
+        auto res = to_bool(value);
+        if (!res.has_value())
+        {
+            return {false, res.error()};
+        }
+        _prune_bind_mounts = res.value();
     }
     else if (key == PRUNE_FS_CONFIG)
     {
-        _prunefs = std::move(value);
+        break_string(_prunefs, value);
     }
     else if (key == PRUNE_PATHS_CONFIG)
     {
-        _prunepaths = std::move(value);
+        break_string(_prunepaths, value);
     }
     else if (key == PRUNE_NAMES_CONFIG)
     {
-        _prunenames = std::move(value);
+        break_string(_prunenames, value);
     }
+
+    return {true, ""};
 }
 
 bool ConfigParser::get_prune_bind_mounts() const noexcept
@@ -124,17 +178,17 @@ bool ConfigParser::get_prune_bind_mounts() const noexcept
     return _prune_bind_mounts;
 }
 
-const std::string &ConfigParser::get_prune_fs() const noexcept
+const std::vector<std::string> &ConfigParser::get_prune_fs() const noexcept
 {
     return _prunefs;
 }
 
-const std::string &ConfigParser::get_prune_paths() const noexcept
+const std::vector<std::string> &ConfigParser::get_prune_paths() const noexcept
 {
     return _prunepaths;
 }
 
-const std::string &ConfigParser::get_prune_names() const noexcept
+const std::vector<std::string> &ConfigParser::get_prune_names() const noexcept
 {
     return _prunenames;
 }
--- Config.h
+++ Config.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 #include "Global.h"
 #include "JsonParser.h"
@@ -21,28 +22,28 @@ public:
     ConfigParser &operator=(const ConfigParser &) noexcept = delete;
     ConfigParser &operator=(ConfigParser &&) noexcept = delete;
 
-    explicit ConfigParser(const std::string &config_file) noexcept;
+    explicit ConfigParser(const std::string &config_file, std::string *err_msg) noexcept;
 
 private:
-    bool parse(const std::string &config_file) noexcept;
+    bool parse(const std::string &config_file, std::string *err_msg) noexcept;
 
     std::pair<bool, std::string> validate_config(const JsonParser &json_data) const noexcept;
 
-    void extract_config(const JsonParser &json_data) noexcept;
+    std::pair<bool, std::string> extract_config(const JsonParser &json_data) noexcept;
 
-    void store_config(const std::string& key, std::string value) noexcept;
+    std::pair<bool, std::string> store_config(const std::string &key, std::string value) noexcept;
 
 public:
     __nodiscard bool get_prune_bind_mounts() const noexcept;
-    __nodiscard const std::string &get_prune_fs() const noexcept;
-    __nodiscard const std::string &get_prune_paths() const noexcept;
-    __nodiscard const std::string &get_prune_names() const noexcept;
+    __nodiscard const std::vector<std::string> &get_prune_fs() const noexcept;
+    __nodiscard const std::vector<std::string> &get_prune_paths() const noexcept;
+    __nodiscard const std::vector<std::string> &get_prune_names() const noexcept;
 
 private:
     bool _prune_bind_mounts;
-    std::string _prunefs;
-    std::string _prunepaths;
-    std::string _prunenames;
+    std::vector<std::string> _prunefs;
+    std::vector<std::string> _prunepaths;
+    std::vector<std::string> _prunenames;
 
     static std::unordered_set<std::string> _valid_configs;
 };
